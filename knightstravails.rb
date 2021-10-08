514ed2b24bc25a91b81a753b19b068ff77ea506d
andrewboles <andrewboles@me.com>
2021-10-07 20:00:23 -0500

    finished

--- knightstravails.rb
+++ knightstravails.rb
@@ -1,56 +1,65 @@
 class Node
-	attr_accessor :moves, :position
+	attr_accessor :moves, :position, :parent_node
 
-	def initialize(position, moves = [])
+	def initialize(position, parent_node = nil, moves = [])
 		@position = position
 		@moves = moves
+		@parent_node = parent_node
 	end
 end
 
 
 class BoardTree
 
-	def knight_move(start_position, end_position, positions = [], move_chain = [start_position])
-		positions << start_position if positions == []
-		# puts "starting positions:"
-		# p "#{positions}"
+	def knight_move(start_position, end_position, positions = [], move_chain = [])
+		if positions == []
+			a = Node.new(start_position)
+			positions << a
+		end
+
 		temp_children =[]
 		positions.size.times do |idx1|
 			temp = create_child_moves(positions[idx1])
 			temp.moves.size.times do |idx2|
 				temp_children << temp.moves[idx2]
 			end
-			if temp.moves.include?(end_position)
-				move_chain << temp.position
-				
-				break
-			end
 		end
 
-		if temp_children.include?(end_position)
-			move_chain << end_position
+		temp_children.delete(nil)
+		temp_children.each do |child|
+				if child.position == end_position
+					puts "found match"
+					next_parent = child.parent_node
+					move_chain << child.position
+					until next_parent == nil do
+						move_chain.unshift(next_parent.position)
+						next_parent = next_parent.parent_node
+					end
+					break
+				end
+			end
+
+		if move_chain != []
 			move_chain
 		else
-			temp_children.delete(nil)
-			knight_move(start_position, end_position, temp_children, move_chain)
+			knight_move(start_position, end_position, temp_children)
 		end
 
 	end
 		
 	def create_child_moves(square)
-
-		a = Node.new(square)
-		a.moves[0] = [square[0]+2,square[1]+1]
-		a.moves[1] = [square[0]+2,square[1]-1]
-		a.moves[2] = [square[0]-2,square[1]+1]
-		a.moves[3] = [square[0]-2,square[1]-1]
-		a.moves[4] = [square[0]+1,square[1]+2]
-		a.moves[5] = [square[0]+1,square[1]-2]
-		a.moves[6] = [square[0]-1,square[1]+2]
-		a.moves[7] = [square[0]-1,square[1]-2]
+		a = square
+		a.moves << Node.new([a.position[0]+2,a.position[1]+1],a)
+		a.moves << Node.new([a.position[0]+2,a.position[1]-1],a)
+		a.moves << Node.new([a.position[0]-2,a.position[1]+1],a)
+		a.moves << Node.new([a.position[0]-2,a.position[1]-1],a)
+		a.moves << Node.new([a.position[0]+1,a.position[1]+2],a)
+		a.moves << Node.new([a.position[0]+1,a.position[1]-2],a)
+		a.moves << Node.new([a.position[0]-1,a.position[1]+2],a)
+		a.moves << Node.new([a.position[0]-1,a.position[1]-2],a)
 
 		8.times do |idx|
-			a.moves[idx] = nil if ((a.moves[idx][0]<0 || a.moves[idx][0]>7) || (a.moves[idx][1]<0 || a.moves[idx][1]>7))
+			a.moves[idx] = nil if ((a.moves[idx].position[0]<0 || a.moves[idx].position[0]>7) || (a.moves[idx].position[1]<0 || a.moves[idx].position[1]>7))
 		end
 
 		a.moves.delete(nil)
@@ -65,4 +74,7 @@ board = BoardTree.new
 
 position_example = [3,6]
 
-p board.knight_move([0,0],[6,4])
+p board.knight_move([0,0],[4,7])
+
+
+
